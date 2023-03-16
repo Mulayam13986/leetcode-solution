ab1c3c8ed2bd71712c1b75a5ea18e090b2e21abd
Mulayam <99270619+Mulayam13986@users.noreply.github.com>
2023-03-17 01:21:49 +0530

    Time: 71 ms (36.64%), Space: 43.2 MB (17.09%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    void print(int arr[], int n){
+        for (int i = 0 ; i < n ; i++){
+            cout << arr[i] << " " ; 
+        }
+        cout << endl; 
+    }
+    int maxScore(vector<int>& cardPoints, int k) {
+        int ans = 0 ; 
+        int front[cardPoints.size()+1] ;  // prefix sum from beginning 
+        int back[cardPoints.size()+1] ;       // peefix sum from end
+        
+        front[0] = 0;
+        for (int i = 0 ; i <  cardPoints.size()  ; i ++ ){
+            front[i+1] = front[i] + cardPoints[i] ; 
+        }
+        
+        back[0] = 0 ; 
+        
+        for (int i = cardPoints.size()-1 ; i >= 0 ; i--){
+            back[cardPoints.size()-i] = cardPoints[i] + back[cardPoints.size()-i-1] ; 
+            
+        }
+        // print(front , cardPoints.size()+1 ) ; 
+        // print(back , cardPoints.size() +1);
+        
+//         we will select i from start and k - i from end ..
+        
+        for (int i = 0 ; i <= k ; i++){
+            
+            ans = max(ans , front[i]+back[k-i]) ;
+        }
+        
+        
+        return ans ;
+    }
+};
