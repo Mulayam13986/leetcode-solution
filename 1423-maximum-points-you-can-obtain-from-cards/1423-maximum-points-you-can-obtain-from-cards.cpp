81e5adfda11fddbc0628b88d4534957bc2939f5f
Mulayam <99270619+Mulayam13986@users.noreply.github.com>
2023-03-17 01:31:03 +0530

    Time: 51 ms (97.67%), Space: 42.4 MB (55.72%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -8,32 +8,48 @@ public:
         cout << endl; 
     }
     int maxScore(vector<int>& cardPoints, int k) {
+//         int ans = 0 ; 
+//         int front[cardPoints.size()+1] ;  // prefix sum from beginning 
+//         int back[cardPoints.size()+1] ;       // peefix sum from end
+        
+//         front[0] = 0;
+//         for (int i = 0 ; i <  cardPoints.size()  ; i ++ ){
+//             front[i+1] = front[i] + cardPoints[i] ; 
+//         }
+        
+//         back[0] = 0 ; 
+        
+//         for (int i = cardPoints.size()-1 ; i >= 0 ; i--){
+//             back[cardPoints.size()-i] = cardPoints[i] + back[cardPoints.size()-i-1] ; 
+            
+//         }
+//         // print(front , cardPoints.size()+1 ) ; 
+//         // print(back , cardPoints.size() +1);
+        
+// //         we will select i from start and k - i from end ..
+        
+//         for (int i = 0 ; i <= k ; i++){
+            
+//             ans = max(ans , front[i]+back[k-i]) ;
+//         }
+        
+        
+//         return ans ;
+        
         int ans = 0 ; 
-        int front[cardPoints.size()+1] ;  // prefix sum from beginning 
-        int back[cardPoints.size()+1] ;       // peefix sum from end
         
-        front[0] = 0;
-        for (int i = 0 ; i <  cardPoints.size()  ; i ++ ){
-            front[i+1] = front[i] + cardPoints[i] ; 
+        for (int i = 0 ; i < k ; i++){
+            ans+=cardPoints[i] ; 
         }
-        
-        back[0] = 0 ; 
-        
-        for (int i = cardPoints.size()-1 ; i >= 0 ; i--){
-            back[cardPoints.size()-i] = cardPoints[i] + back[cardPoints.size()-i-1] ; 
+        int res = ans ; 
+        int j = cardPoints.size()-1 ; 
+        for (int i = k-1 ; i >=0; i-- ){
             
+            ans -= cardPoints[i];
+            ans+= cardPoints[j] ; 
+            j-- ;
+            res = max(res , ans);
         }
-        // print(front , cardPoints.size()+1 ) ; 
-        // print(back , cardPoints.size() +1);
-        
-//         we will select i from start and k - i from end ..
-        
-        for (int i = 0 ; i <= k ; i++){
-            
-            ans = max(ans , front[i]+back[k-i]) ;
-        }
-        
-        
-        return ans ;
+        return res ; 
     }
 };
